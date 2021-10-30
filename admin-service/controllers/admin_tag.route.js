0111725c3d8ca56bc9cca330bb57390e1e4787f6
l4zyc0d3r <huytrquang1103@gmail.com>
2021-10-30 17:37:57 +0700

    Refactor admin_tag complete

--- admin-service/controllers/admin_tag.route.js
+++ admin-service/controllers/admin_tag.route.js
@@ -1,78 +1,46 @@
 const express = require('express');
 const tagModel = require('../models/tag.model');
-const {checkAuthenticated,isAdmin} = require('../models/user.model');
 const router = express.Router();
 
-
-router.get('/',checkAuthenticated,isAdmin, async function (req, res) {
-
+router.get('/', async function (req, res) {
     const tags = await tagModel.all();
 
-    res.render('vwAdmin/tags', {
-        layout: 'admin.hbs',
-        tagActive: true,
-        tags
-    });
-})
-router.get('/tags/add',checkAuthenticated,isAdmin, async function (req, res) {
+    return res.json({ tags });
+});
 
-    res.render('vwAdmin/addTag', {
-        layout: 'admin.hbs',
-        tagActive: true
-
-    });
-})
 router.post('/tags/add', async function (req, res) {
-
-    const tag = req.body.tag_name;
+    const tag = JSON.parse(decodeURI(req.query.tagName));
     await tagModel.add(tag);
+    return res.json(true);
+});
 
-    res.redirect('/admin/tags/add');
-})
+router.get(
+    '/tags/edit',
 
-router.get('/is-tag-available',checkAuthenticated,isAdmin, async function (req, res) {
-    const tag_name = req.query.tag_name;
-    const list = await tagModel.all();
-    let tags = []
-    for (c of list) {
-        tags.push(c.tag_name.toLowerCase());
+    async function (req, res) {
+        const tagID = +req.query.tagID;
+        const tagDetail = await tagModel.findByID(tagID);
+
+        if (tagDetail === null) {
+            return res.json({ result: false });
+        }
+
+        return res.json({ result: true, tagDetail });
     }
-
-    res.json(!tags.includes(tag_name.toLowerCase()));
-})
-
-router.get('/tags/edit',checkAuthenticated,isAdmin, async function (req, res) {
-
-    const tag_id = req.query.id;
-    const tagDetail = await tagModel.findByID(tag_id);
-
-    if (tagDetail === null) {
-        return res.redirect('/admin');
-    }
-    res.render('vwAdmin/editTag', {
-        layout: 'admin.hbs',
-        tagActive: true,
-        tagDetail
-    });
-})
+);
 
 router.post('/tags/patch', async function (req, res) {
-
-    const updatedTag = {
-        id: req.query.id,
-        tag_name: req.body.tag_name
-    } 
-
+    const updatedTag = JSON.parse(decodeURI(req.query.updatedTag));
     await tagModel.patch(updatedTag);
 
-    res.redirect('/admin');
-})
+    return res.json(true);
+});
 
 router.post('/tags/del', async function (req, res) {
+    const tagID = +req.query.tagID;
+    await tagModel.del(tagID);
 
-    await tagModel.del(req.query.id);
-
-    res.redirect('/admin');
-})
+    return res.json(true);
+});
 
 module.exports = router;
