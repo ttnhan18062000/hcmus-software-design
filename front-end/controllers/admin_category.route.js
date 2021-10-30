a78fb6c0a45ae198049c494730ef088db2f2c6f4
l4zyc0d3r <huytrquang1103@gmail.com>
2021-10-30 17:21:05 +0700

    Refactor admin_category complete

--- front-end/controllers/admin_category.route.js
+++ front-end/controllers/admin_category.route.js
@@ -1,85 +1,120 @@
 const express = require('express');
 const categoryModel = require('../models/category.model');
 const {checkAuthenticated,isAdmin} = require('../models/user.model');
-
+const { HTTP } = require('http-call');
+const adminURL = require('../../configs/services-url.json')['admin-service'];
 const router = express.Router();
 
-router.get('/categories',checkAuthenticated,isAdmin, async function (req, res) {
 
+
+router.get('/categories', checkAuthenticated, isAdmin, function (req, res) {
     res.render('vwAdmin/categories', {
         layout: 'admin.hbs',
         categoryActive: true,
     });
-})
+});
 
-router.get('/categories/add',checkAuthenticated,isAdmin, async function (req, res) {
-    
-    res.render('vwAdmin/addCategory', {
-        layout: 'admin.hbs',
-        categoryActive: true,
-    });
-})
-
-router.get('/is-category-available',checkAuthenticated,isAdmin, async function (req, res) {
-    const title = req.query.title;
-    const parent_id = req.query.parent_id;
-    const list = await categoryModel.findByParentID(parent_id);
-    let titles = []
-    for (c of list) {
-        titles.push(c.title.toLowerCase());
+router.get(
+    '/categories/add',
+    checkAuthenticated,
+    isAdmin,
+    async function (req, res) {
+        res.render('vwAdmin/addCategory', {
+            layout: 'admin.hbs',
+            categoryActive: true,
+        });
     }
+);
 
-    res.json(!titles.includes(title.toLowerCase()));
-})
+router.get(
+    '/is-category-available',
+
+    async function (req, res) {
+        const title = req.query.title;
+        const parent_id = req.query.parent_id;
+        const list = await categoryModel.findByParentID(parent_id);
+        let titles = [];
+        for (c of list) {
+            titles.push(c.title.toLowerCase());
+        }
+
+        res.json(!titles.includes(title.toLowerCase()));
+    }
+);
 
 router.post('/categories/add', async function (req, res) {
-    const new_category = req.body;
-    await categoryModel.add(new_category);
-    
+    const newCategory = req.body;
+    await HTTP.post(
+        encodeURI(
+            adminURL +
+                '/admin/categories/add?newCategory=' +
+                JSON.stringify(newCategory)
+        )
+    );
     res.redirect('/admin/categories/add');
-})
+});
 
-router.get('/categories/edit',checkAuthenticated,isAdmin, async function (req, res) {
+router.get(
+    '/categories/edit',
+    checkAuthenticated,
+    isAdmin,
+    async function (req, res) {
+        const categoryId = req.query.id;
+        const { body: data } = await HTTP.get(
+            encodeURI(
+                adminURL + '/admin/categories/edit?categoryId=' + categoryId
+            )
+        );
 
-    const category_id = req.query.id;
-    const categoryDetail = await categoryModel.findByID(category_id);
+        if (!data.result) {
+            return res.redirect('/admin/categories');
+        }
+        const categoryDetail = data.categoryDetail;
 
-    if (categoryDetail === null) {
-        return res.redirect('/admin/categories');
+        const isNotParent =
+            categoryDetail.parent_title === 'Không' ? false : true;
+
+        res.render('vwAdmin/editCategory', {
+            layout: 'admin.hbs',
+            categoryActive: true,
+            categoryDetail,
+            isNotParent,
+        });
     }
-    
-
-    const isNotParent = categoryDetail.parent_title === 'Không' ? false : true;
-
-    res.render('vwAdmin/editCategory', {
-        layout: 'admin.hbs',
-        categoryActive: true,
-        categoryDetail,
-        isNotParent
-    });
-})
+);
 
 router.post('/categories/patch', async function (req, res) {
-
-    const updatedCategory ={
+    const updatedCategory = {
         id: req.query.id,
-        title: req.body.title
-    }
+        title: req.body.title,
+    };
 
-    await categoryModel.patch(updatedCategory);
+    await HTTP.post(
+        encodeURI(
+            adminURL +
+                '/admin/categories/patch?updatedCategory=' +
+                JSON.stringify(updatedCategory)
+        )
+    );
 
     res.redirect('/admin/categories');
-})
+});
 
 router.post('/categories/del', async function (req, res) {
-    
     const catID = req.query.id;
     const catParentID = req.query.parent_id;
 
-    await categoryModel.del(catID,catParentID);
+    await HTTP.post(
+        encodeURI(
+            adminURL +
+                '/admin/categories/del?catID=' +
+                catID +
+                '&catParentID=' +
+                catParentID
+        )
+    );
 
     res.redirect('/admin/categories');
-})
-
+});
 
 module.exports = router;
