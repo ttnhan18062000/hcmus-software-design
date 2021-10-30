f4cac51a040b8c71a9971b02607b8028bcebe4d2
l4zyc0d3r <huytrquang1103@gmail.com>
2021-10-30 18:17:40 +0700

    remove database

--- admin-service/database/subscribe.sql
+++
@@ -1,9 +0,0 @@
-use newspaperdb;
-
-create table pending_subscribe(
-	id int unsigned auto_increment,
-    userID int unsigned,
-    days_subscribe int unsigned not null,
-    primary key (id),
-    foreign key (userID) references Users(id)
-);
